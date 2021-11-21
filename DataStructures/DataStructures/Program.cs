a274e2fe35cf9950cce9411ea4f3654593c93270
Hitesh <hiteshspatil99@gmail.com>
2021-11-21 23:26:28 +0530

    [Hitesh] add : Created Simple Linked List

---
+++ DataStructures/DataStructures/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            LinkedList link = new LinkedList();
+            link.Add(56);
+            link.Add(30);
+            link.Add(70);
+            link.Display();
+        }
+    }
+}
+
