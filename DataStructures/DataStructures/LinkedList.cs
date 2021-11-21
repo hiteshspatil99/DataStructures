58e0dac025b4dc3ec0808139278ab3da0b070f53
Hitesh <hiteshspatil99@gmail.com>
2021-11-21 23:43:46 +0530

    [Hitesh] add : Created Linked List BY Appending

--- DataStructures/DataStructures/LinkedList.cs
+++ DataStructures/DataStructures/LinkedList.cs
@@ -7,10 +7,6 @@ namespace DataStructures
     public class LinkedList
     {
         internal Node head;
-        ///<summary>
-        ///UC-1
-        /// </summary>
-        ///<param name="data"></param>
         internal void Add(int data)
         {
             Node node = new Node(data);
@@ -29,6 +25,45 @@ namespace DataStructures
             }
             Console.WriteLine("{0} Inserted into Linked List", node.data);
         }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked List is Empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+        internal Node InsertAtParticularPosition(int position, int data)
+        {
+            Node newestNode = new Node(data);
+            if (this.head == null)
+            {
+                return newestNode;
+            }
+            if (position == 0)
+            {
+                newestNode.next = this.head;
+                this.head = newestNode;
+                return this.head;
+            }
+            Node prev = null;
+            Node current = this.head;
+            int count = 0;
+            while (current != null && count < position)
+            {
+                prev = current;
+                current = current.next;
+                count++;
+            }
+            newestNode.next = prev.next;
+            prev.next = newestNode;
+            return this.head;
+        }
     }
-}
 }
--- DataStructures/DataStructures/Program.cs
+++ DataStructures/DataStructures/Program.cs
@@ -11,6 +11,8 @@ namespace DataStructures
             link.Add(56);
             link.Add(30);
             link.Add(70);
+            link.InsertAtParticularPosition(2, 30);
+            link.Display();
         }
     }
 }
