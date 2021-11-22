faa3c218f9ae48cc6c91ebbcae1424f42e6d2aaa
Hitesh <hiteshspatil99@gmail.com>
2021-11-23 01:08:00 +0530

    [Hitesh] add : Create Stack Peak And Pop From Stack

--- DataStructures/DataStructures/Program.cs
+++ DataStructures/DataStructures/Program.cs
@@ -7,13 +7,11 @@ namespace DataStructures
         static void Main(string[] args)
         {
 
-            LinkedList link = new LinkedList();
-            link.Add(56);
-            link.Add(30);
-            link.Add(70);
-            link.Display();
-            int position = link.Search(40);
-            link.DeleteNodeAtParticularPosition(position);
+            Queue que = new Queue();
+            que.Enqueue(70);
+            que.Enqueue(30);
+            que.Enqueue(56);
+            que.Display();
         }
     }
 }
